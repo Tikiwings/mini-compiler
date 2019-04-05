3b056179dd3fca8581798f322c1d005b6a2a7899
dsanch65 <donaldsanchez7870@gmail.com>
2019-04-05 13:13:45 -0700

    added in some parsing for struct/types and global declarations

--- validator.py
+++ validator.py
@@ -2,9 +2,42 @@
 import json
 import sys
 
-def main():
+types = ("int", "bool", "struct id")
+structTable = []
+
+def makeStructTable(prog):
+   for struct in prog["types"]:
+      print(struct)
+      print("\n\n")
+      structTable.append(struct["id"])
+
+def getGlobalDecls(prog, symTable):
+   for ID in prog["declarations"]:
+      symTable.append(ID)
+
+#def checkFuncs(prog):
+
+def createJson():
    with open(sys.argv[1], "r") as f:
       progFile = json.load(f)
-   print(json.dumps(progFile, indent=4))
+   #print(json.dumps(progFile, indent=4))
+   return progFile
+
+def main():
+   symbolTable = []
+   progFile = createJson()
+
+   makeStructTable(progFile)
+   getGlobalDecls(progFile, symbolTable)
+
+   print(structTable)
+   print(symbolTable)
+
+   ''' for x,y in progFile.items():
+      for p in progFile[x]:
+         print(p)
+      print("=======================================================")
+   '''
+
 
 main()
